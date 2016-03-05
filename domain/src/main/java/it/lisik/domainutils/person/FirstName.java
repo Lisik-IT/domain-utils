78ce339925c33fdd2a9ee5d2ed00e89f0bf28fc0
Mateusz <mateusz@lisik.it>
2016-03-05 18:44:26 +0100

    Name related value object

---
+++ domain/src/main/java/it/lisik/domainutils/person/FirstName.java
@@ -0,0 +1,38 @@
+package it.lisik.domainutils.person;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Range;
+import it.lisik.domainutils.ValueObject;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+public class FirstName implements ValueObject<String> {
+    public final static Range<Integer> NAME_RANGE = Range.closed(1, 100);
+    private final String firstName;
+
+    /**
+     * Build first name value object out of string
+     *
+     * @param firstName first name
+     * @throws FirstNameLengthException when name doesn't match constrain [1,100]
+     */
+    public FirstName(@NotNull String firstName) {
+        firstName = StringUtils.stripToNull(firstName);
+        firstName = Preconditions.checkNotNull(firstName);
+
+        if (!NAME_RANGE.contains(firstName.length())) {
+            throw new FirstNameLengthException(firstName, NAME_RANGE);
+        }
+
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String getValue() {
+        return firstName;
+    }
+}
---
+++ domain/src/main/java/it/lisik/domainutils/person/LastName.java
@@ -0,0 +1,38 @@
+package it.lisik.domainutils.person;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Range;
+import it.lisik.domainutils.ValueObject;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+public class LastName implements ValueObject<String> {
+    public final static Range<Integer> NAME_RANGE = Range.closed(1, 100);
+    private final String lastName;
+
+    /**
+     * Build LastName (Surname) value object out of string
+     *
+     * @param lastName Last Name
+     * @throws LastNameLengthException when name doesn't match constrain [1,100]
+     */
+    public LastName(@NotNull String lastName) {
+        lastName = StringUtils.stripToNull(lastName);
+        lastName = Preconditions.checkNotNull(lastName);
+
+        if (!NAME_RANGE.contains(lastName.length())) {
+            throw new LastNameLengthException(lastName, NAME_RANGE);
+        }
+
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String getValue() {
+        return lastName;
+    }
+}
