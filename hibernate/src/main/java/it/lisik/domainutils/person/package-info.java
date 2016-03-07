be3ce0eea9639108741b68056a0fd0a1b9a79dea
Mateusz <mateusz@lisik.it>
2016-03-07 13:40:40 +0100

    Added typedef for Name VO

--- hibernate/src/main/java/it/lisik/domainutils/person/package-info.java
+++ hibernate/src/main/java/it/lisik/domainutils/person/package-info.java
@@ -1,6 +1,7 @@
 @TypeDefs({
         @TypeDef(typeClass = FirstNameType.class, defaultForType = FirstName.class, name = "firstname"),
-        @TypeDef(typeClass = LastNameType.class, defaultForType = LastName.class, name = "lastname")
+        @TypeDef(typeClass = LastNameType.class, defaultForType = LastName.class, name = "lastname"),
+        @TypeDef(typeClass = NameType.class, defaultForType = Name.class, name = "name")
 })
 package it.lisik.domainutils.person;
 
